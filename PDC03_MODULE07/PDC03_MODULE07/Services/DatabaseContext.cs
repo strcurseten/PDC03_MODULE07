76bb99966ae9a67fbb794bce066cfb55e649f5ec
ten <fernandez.kirstenpearl@auf.edu.ph>
2023-06-02 13:24:27 +0800

    first commit

---
+++ PDC03_MODULE07/PDC03_MODULE07/Services/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using PDC03_MODULE07.Model;
+using System.IO;
+
+namespace PDC03_MODULE07.Services
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<EmployeeModel> Employee { get; set; }
+        public DatabaseContext() 
+        { 
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Employee.db");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
---
+++ PDC03_MODULE07/PDC03_MODULE07/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using PDC03_MODULE07.Model;
+
+using System.Threading.Tasks;
+
+
+namespace PDC03_MODULE07.ViewModel
+
+{
+    public class EmployeeViewModel
+    {
+        //Call Database
+
+        private Services.DatabaseContext getContext()
+        {
+            return new Services.DatabaseContext();
+        }
+
+
+        //Insert Records
+
+        public int InsertEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employee.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        //Retrieve Records
+
+        public async Task<List<EmployeeModel>>GetAllEmployees()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Employee.ToListAsync();
+            return res;
+        }
+
+        //Delete Records
+
+        public int DeleteEmployee(EmployeeModel obj) 
+        {
+            var _dbContext = getContext();
+            _dbContext.Employee.Remove(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        //Update Records
+
+        public async Task<int> UpdateEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employee.Update(obj);
+            int c = await _dbContext.SaveChangesAsync();
+            return c;
+        }
+
+
+    }
+}
